22069ffcf68a5fe59e00e9f9cf071617e4f0df12
Pyungchul Kim <142457381+peterkim1963@users.noreply.github.com>
2026-05-23 16:03:44 -0700

    Initial commit

---
+++ view.v
@@ -0,0 +1,12 @@
+/* NOTE: DO NOT ADD COMMENTS AT THE END OF A VALID CONTROL.
+ * IT WILL CAUSE A PARSING ERROR.
+ */
+corrected rotation: on
+z buffer: on
+/* For some reason, the contour sides get trimmed at higher number
+ * when line style is pixel or polygon. Maybe, contour info gets fully
+ * populated only when line style is cylinder.
+ */
+line style: cylinder
+render mode: shaded
+projection: parallel
