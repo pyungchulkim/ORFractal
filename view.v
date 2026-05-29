ca2b3ad23177522bfdc58032c7bcdbba7279e939
Pyungchul Kim <142457381+peterkim1963@users.noreply.github.com>
2026-05-29 11:56:31 -0700

    refactoring for parameterized simulation and many fixes

--- view.v
+++ view.v
@@ -3,6 +3,8 @@
  */
 corrected rotation: on
 z buffer: on
+concave polygons: on
+
 /* For some reason, the contour sides get trimmed at higher number
  * when line style is pixel or polygon. Maybe, contour info gets fully
  * populated only when line style is cylinder.
